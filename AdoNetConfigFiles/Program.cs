02a984967ec1d395eb007d6f475c7d428e399b45
YuriiTrofimenko <tyaa@ukr.net>
2021-05-21 21:16:24 +0300

    json config

--- AdoNetConfigFiles/Program.cs
+++ AdoNetConfigFiles/Program.cs
@@ -1,9 +1,45 @@
 ﻿using System;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace AdoNetConfigFiles
 {
+    /* public static class Ext
+    {
+        public static string ExtMethod(this IData obj, string data)
+        {
+            if (obj is Person)
+            {
+                return ((Person)obj).name + " " + ((Person)obj).lastName + " " + data;
+            }
+            if (obj is Product)
+            {
+                return ((Product)obj).title + " " + ((Product)obj).price;
+            }
+            return "";
+        }
+    }
+
+    public interface IData
+    {
+        
+    }
+
+    public class Person : IData
+    {
+        public string name;
+        public string lastName;
+    }
+    
+    class Product : IData
+    {
+        public string title;
+        public Decimal price;
+    } */
+
     class Program
     {
         static void Main(string[] args)
@@ -11,18 +47,44 @@ namespace AdoNetConfigFiles
             // Console.WriteLine("Hello World!");
             // Console.WriteLine("Connection String 'docker-mssql:'");
             // Console.WriteLine(ConfigurationManager.ConnectionStrings["docker-mssql"].ConnectionString);
-            SqlConnectionStringBuilder builder =
-                new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["docker-mssql-wo-credentials"].ConnectionString);
-            builder.UserID = ConfigurationManager.AppSettings["user-name"];
-            builder.Password = ConfigurationManager.AppSettings["password"];
-            Console.WriteLine(builder.ConnectionString);
-            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            
+            // Console.WriteLine(readConnectionStringOld());
+            // using (SqlConnection connection = new SqlConnection(readConnectionStringOld()))
+            
+            /* Console.WriteLine(readConnectionStringNew());
+            using (SqlConnection connection = new SqlConnection(readConnectionStringNew()))
             {
                 connection.Open();
                 SqlCommand testSelectCommand = connection.CreateCommand();
                 testSelectCommand.CommandText = "SELECT COUNT(*) FROM [TotalStat]";
                 Console.WriteLine(testSelectCommand.ExecuteScalar());
-            }
+            } */
+
+            /* var person = new Person() { name = "Noname", lastName = "Last"};
+            var product = new Product() { title = "PC", price = 40000};
+            Console.WriteLine(person.ExtMethod("demo data 1"));
+            Console.WriteLine(product.ExtMethod("demo data 2")); */
+            
+            Console.WriteLine(ReadConnectionStringNew());
+        }
+
+        private static string ReadConnectionStringOld()
+        {
+            SqlConnectionStringBuilder builder =
+                new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["docker-mssql-wo-credentials"].ConnectionString);
+            builder.UserID = ConfigurationManager.AppSettings["user-name"];
+            builder.Password = ConfigurationManager.AppSettings["password"];
+            return builder.ConnectionString;
+        }
+
+        private static string ReadConnectionStringNew()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+            return config.GetSection("Data").GetSection("password").Value;
         }
     }
 }
