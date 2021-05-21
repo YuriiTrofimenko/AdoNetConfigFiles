e560028825524a4e388ac03c78b7b3c6aa6abd4f
YuriiTrofimenko <tyaa@ukr.net>
2021-05-21 19:04:55 +0300

    init

---
+++ AdoNetConfigFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AdoNetConfigFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("Connection String 'docker-mssql:'");
+            // Console.WriteLine(ConfigurationManager.ConnectionStrings["docker-mssql"].ConnectionString);
+            SqlConnectionStringBuilder builder =
+                new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["docker-mssql-wo-credentials"].ConnectionString);
+            builder.UserID = ConfigurationManager.AppSettings["user-name"];
+            builder.Password = ConfigurationManager.AppSettings["password"];
+            Console.WriteLine(builder.ConnectionString);
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                SqlCommand testSelectCommand = connection.CreateCommand();
+                testSelectCommand.CommandText = "SELECT COUNT(*) FROM [TotalStat]";
+                Console.WriteLine(testSelectCommand.ExecuteScalar());
+            }
+        }
+    }
+}
